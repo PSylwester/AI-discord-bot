d9baee8c24ec48deedc2d606bcd3b2a811e887cd
PSylwester <p.sylwek18@wp.pl>
2025-01-24 02:03:01 +0100

    hotfix_3

--- Project/main.py
+++ Project/main.py
@@ -7,18 +7,33 @@ from gtts import gTTS
 from google.cloud import translate_v2 as translate
 import html
 import json
-from discord.ext import commands
 from apikeys import CHATBOTTOKEN
 import ollama
 import requests
-from discord.ext import commands
 from youtube_transcript_api import YouTubeTranscriptApi
 import tiktoken
 from collections import deque
 import spacy
 import re
 
+
+
+from ChatBot.ChatBot import setup_chatbot_commands
+from TranslateBot.TranslateBot import setup_translate_commands
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"googlekey.json"
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+# Tworzenie instancji bota
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
+# Dodawanie funkcjonalności z innych modułów
+setup_chatbot_commands(bot)
+setup_translate_commands(bot)
+# Event: uruchomienie bota
+@bot.event
+async def on_ready():
+    print(f"Bot is online as {bot.user.name}")
+
+# Uruchomienie bota
+bot.run(CHATBOTTOKEN)
 
